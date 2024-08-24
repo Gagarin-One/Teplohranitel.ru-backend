af9abd4d4ceea1cef82e644c7fa64dbfaeaabdaa
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2024-08-25 00:26:01 +0300

    fix url

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://teplohranitel-avto.ru', // замените на URL вашего клиента
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // разрешенные методы
+}))
 app.use(express.json())
 
 
